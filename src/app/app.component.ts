0272bfcfc37b7f739cb25d9d7cb58bcdcb2a186a
Matthew Waskin <matthew@waskin.com>
2023-06-10 20:27:26 -0400

    used Angular CLI to scaffold project; learned basics of creating and nesting Angular components; lessons 1-23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+/* Angular-specific TS decorator (imported) that provides metadata to be interpretted by Angular when the exported Component is instantiated */
 @Component({
+  /* Essentially creates unique HTML tag (CSS selector) by this name that triggers Angular to render the component when instantiated in a component's HTML template */
   selector: 'app-root',
+  /* path of the HTML template for this component; can also use 'template' property to write the template inline as a string (backticks for multi-line) */
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /* path(s) of the style sheet(s) for this component; can also use 'styles' property to write the styling inline as a string (backticks for multi-line) */
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-first-app';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
+/* Angular class decorator that provides configuration metadata to the AppModule bootstrapped in main.ts */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  /* Any components instantiated in the app's component tree must be registered in the declarations array */
+  declarations: [AppComponent, ServerComponent, ServersComponent],
+  /* Access functionality of other modules, such as those built into Angular */
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  /* Tells Angular which component should be rendered when the app starts; basically which component's selector Angular should look for in the root index.html file */
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
